e4b545aab73345d40d39cd3cbea10e0b72e361f3
diennguyen2611 <you@example.com>
2021-10-29 23:24:18 +0700

    tạo giao diện chi tiết tour

---
+++ guess/tourdetails.php
@@ -0,0 +1,92 @@
+<?php
+include('header.php');
+?>
+
+<div class="my-tourdetails" >
+    <div class="row text-uppercase">
+        <h1>COMBO TRỌN GÓI HỒI HƯƠNG TỪ CANADA</h1>
+    </div>
+    <div class="col-md-7">
+        <div class="my-tour">
+            <div class="bg">
+            </div>
+            <div class="my-intro">
+
+                <span class=""><i class="fas fa-map-marker-alt"></i><span>Canada</span></span>
+                <span class=""><i class="far fa-clock"></i><span>2 Ngày 1 Đêm</span></span>
+                <span class="">Phương tiện: <span><i class="fas fa-plane"></i></span></span>
+                <span class="float-end">Mã Tour: <span class="text-primary">T001</span></span>
+
+
+            </div>
+        </div>
+
+        <div class="my-tourdes">
+            <h2>
+                COMBO TRỌN GÓI HỒI HƯƠNG TỪ CANADA
+            </h2>
+
+            <p>
+                Trong tình hình dịch Covid 19 đang rất căng thẳng trên toàn thế giới, nhu cầu được quay trở về Việt Nam
+                sinh sống và làm việc trong cộng đồng người Việt Nam ở nước ngoài đang rất cao. Các đối tượng được ưu
+                tiên hồi hương trong thời điểm này bao gồm học sinh dưới 18 tuổi, sinh viên đã hoàn thành khóa học gặp
+                khó khăn về chỗ ở và việc làm, người lao động hết hạn hợp đồng, mất việc, không còn thu nhập mà nước sở
+                tại không đủ điều kiện hỗ trợ, khách du lịch, thương nhân, trí thức mắc kẹt tại nước ngoài do Covid 19,
+                gặp khó khăn do không có nơi ở, không còn khả năng tài chính.
+            </p>
+            <p>
+                Nếu quý khách đang có dự định hoặc cần đăng ký cho người thân trở về Việt Nam tại thời điểm này, hãy
+                liên hệ với IVIVU.COM để được tư vấn nhanh nhất, hiệu quả nhất.
+            </p>
+
+
+
+            <div class="my-tourSchedule">
+                <h3>Chương trình tour</h3>
+                Để đảm bảo có thể hồi hương Việt Nam thành công trong thời điểm Covid 19 này, quý khách có nguyện vọng
+                hồi
+                hương cần chuẩn bị và cung cấp đầy đủ các giấy tờ sau:
+
+                - Passport Việt Nam còn hạn ít nhất 06 tháng tính đến ngày bay (bản scan có màu).
+
+                - Giấy chứng nhận tiêm đủ 2 mũi vaccine hoặc giấy xác nhận khỏi bệnh Covid theo đúng quy định.
+
+                - Kết quả test COVID-19 bằng phương pháp RT-PCR/RT-LAMP.
+
+                - Bản cam kết phòng chống dịch COVID-19.
+
+                QUY TRÌNH HỒI HƯƠNG VỀ VIỆT NAM
+
+                - Thực hiện xét nghiệm Covid tại nước sở tại
+                theo phương pháp Real-time PRC trong vòng 72 giờ trước giờ bay
+                (IVIVU sẽ được thông báo về thời điểm chính xác để quý khách đi lấy mẫu xét nghiệm).
+
+                - Đến ngày bay, quý khách chuẩn bị đầy đủ giấy tờ và di chuyển đến sân bay để thực hiện chuyến bay. Sau
+                khi
+                đáp chuyến bay về đến Việt Nam, quý khách thực hiện khai báo y tế, nhân viên y tế tiến hành kiểm tra và
+                xét
+                nghiệm đồng thời hỗ trợ các thủ tục nhập cảnh, với những hành khách có sức khỏe ổn định sẽ được đưa về
+                khách
+                sạn để tiến hành cách ly.
+
+                - Trong quá trình cách ly tại Khách Sạn, quý khách sẽ được phục vụ ăn 03 bữa, nghỉ tại khách sạn tiêu
+                chuẩn
+                02 người/ phòng (nếu quý khách có nhu cầu đăng ký phòng đơn sẽ có phụ thu). Tại Khách Sạn, quý khách sẽ
+                được
+                xét nghiệm kiểm tra COVID-19 theo quy định.
+
+                - Sau khi hoàn thành thời gian cách ly tại khách sạn và kiểm tra sức khỏe theo yêu cầu của Bộ Y Tế, quý
+                khách sẽ được về nơi lưu trú, nhưng sẽ tiếp tục tự cách ly tại nhà thêm 07 hoặc 14 ngày tùy quy định của
+                từng địa phương
+
+            </div>
+        </div>
+    </div>
+    <div class="col-md-5">
+    </div>
+</div>
+
+
+<?php 
+include('footer.php');
+?>
