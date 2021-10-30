c957f2a890320dd358ba4a7b471f914d970f86da
thuha-286 <nthuha0608@gmail.com>
2021-10-30 13:27:18 +0700

    update

---
+++ guess/tailor-tour.php
@@ -0,0 +1,61 @@
+<?php
+  include('header.php');
+?>
+
+<div class="tailor-tour" style="overflow: hidden">
+    <div class="container">
+        <div class="banner-content-th col-md-5">
+            <h3 class="">Tailor Tour</h3>
+            <p>Với kiến thức chuyên môn của chúng tôi, chúng tôi sẽ cung cấp cho bạn 1 chuyến du lịch trải nghiệm đầy
+                thú vị. Mỗi hành trình được thiết kế riêng đều được sắp xếp cẩn thận để phù hợp với nhu cầu và kinh phí
+                của bạn. Chúng tôi có thể tư vấn khi nào là thời điểm phù hợp để đi du lịch, các tuyến điểm đẹp nhất để
+                đi và khách sạn tốt nhât để ở. Bạn có thể sử dụng các chương trình như iVIVU có trên Website nhưng
+                điều chỉnh lại cho phù hợp với nhu cầu của bạn, hoặc đơn giản là thiết kế chuyến đi của riêng bạn.
+            </p>
+            <p>Để được tư vấn thêm, vui lòng gọi (028) 3933 8002 để nói chuyện với một trong những cố vấn du lịch chuyên
+                nghiệp của iVIVU, các bạn đang rất sẵn lòng hỗ trợ.
+            </p>
+
+            <a href="#" class="btn btn-th">Liên hệ ngay</a>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+
+    <div class="container">
+        <div class="row text-center tailor-service-th">
+            <div class="col-md-3 col-th">
+                <div class="tailor-item">
+                    <img src="../img/tailor-service1.png" alt="" class="img-tailor-th">
+                    <p>Thiết kế hành trình riêng</p>
+
+                </div>
+            </div>
+            <div class="col-md-3 col-th">
+                <div class="tailor-item">
+                    <img src="../img/tailor-service2.png" alt="" class="img-tailor-th">
+                    <p>Dịch vụ theo nhu cầu</p>
+                </div>
+            </div>
+            <div class="col-md-3 col-th">
+                <div class="tailor-item">
+
+                    <img src="../img/tailor-service3.png" alt="" class="img-tailor-th">
+                    <p>Thời gian linh hoạt</p>
+                </div>
+            </div>
+            <div class="col-md-3 col-th">
+                <div class="tailor-item">
+
+                    <img src="../img/tailor-service4.jpg" alt="" class="img-tailor-th">
+                    <p>Có đội ngũ tư vấn riêng</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+  include('footer.php');
+?>
