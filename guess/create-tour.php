9dd595769d917ebda682cf5b06db67601bde1c4c
thuha-286 <nthuha0608@gmail.com>
2021-10-30 09:19:26 +0700

    a

--- guess/create-tour.php
+++ guess/create-tour.php
@@ -2,8 +2,30 @@
   include('header.php');
 ?>
 
-<div class="create-tour">
+<div class="create-tour" style="overflow: hidden">
+    <div class="row">
+        <div class="col-md-1"></div>
+        <div class="my-banner-content col-md-6 rounded">
+            <h1 class="text-white">Du lịch theo phong cách riêng</h1>
+            <h5 class="text-white"><i>Du lịch trọn vẹn, giá cả phải chăng</i></h5>
+            <div class="my-search">
+                <div class="input-group mb-3">
 
+                    <input type="text" class="form-control" placeholder="Bạn muốn đi đâu"
+                        aria-label="Recipient's username" aria-describedby="button-addon2">
+                    <button class="btn my-btn-search" type="button" id="button-addon2">Tìm Kiếm</button>
+                </div>
+            </div>
+            Tailor tour - Trải nghiệm theo cách riêng của bạn -
+            <a href="create-tour.php" class="btn btn-outline-light">Đặt tour riêng</a>
+        </div>
+        <div class="col-md-4 my-hottour text-white">
+            <h3>Tour Cô Tô 3N2D</h3>
+            <h4>Du lịch miền cát trắng</h4>
+            <h4>Giá chỉ từ</h4>
+            <h1>2300000VNĐ</h1>
+        </div>
+    </div>
 </div>
 
 <?php
