f60b20ce47a653eb6bf94723e2373c16bc570587
nguyenthikhanhhuyen <nguyent.khanhhuyen309@gmail.com>
2021-10-30 14:46:33 +0700

    gd moretour

--- guess/moretour.php
+++ guess/moretour.php
@@ -22,8 +22,7 @@ include('header.php');?>
                 </div>
                 <div class="col-md-5">
                     <div class="row">
-                        <a href=""
-                            class="linkdetail">
+                        <a href="" class="linkdetail">
                             Tour Quy Nhơn 1N: Đảo Ngọc Cù Lao Xanh
                         </a>
                     </div>
@@ -39,12 +38,12 @@ include('header.php');?>
                         <div class="col-md-1"><i class="fas fa-plane"></i></div>
                     </div>
                     <div class="row  list_moretour">
-                                <ul>
-                                    <li>Bữa Trưa Hải Sản</li>
-                                    <li>Bãi đá Thảo Nguyên</li>
-                                    <li>Ngọn Hải Đăng 120 tuổi</li>
-                                </ul>
-                            </div>
+                        <ul>
+                            <li>Bữa Trưa Hải Sản</li>
+                            <li>Bãi đá Thảo Nguyên</li>
+                            <li>Ngọn Hải Đăng 120 tuổi</li>
+                        </ul>
+                    </div>
                 </div>
                 <div class="col text-end ">
                     <p class="text-gray">Hết chỗ</p>
@@ -58,9 +57,8 @@ include('header.php');?>
                 </div>
                 <div class="col-md-5">
                     <div class="row">
-                        <a href=""
-                            class="linkdetail">
-                            Tour Cù Lao Chàm 1N: Đi Bộ Dưới Đáy Biển Cano                                                        
+                        <a href="" class="linkdetail">
+                            Tour Cù Lao Chàm 1N: Đi Bộ Dưới Đáy Biển Cano
                         </a>
                     </div>
                     <div class="row">
@@ -75,12 +73,12 @@ include('header.php');?>
                         <div class="col-md-1"><i class="fas fa-subway"></i></div>
                     </div>
                     <div class="row  list_moretour">
-                                <ul>
-                                    <li>Lướt cano trên biển</li>
-                                    <li>Ngắm San Hô</li>
-                                    <li>Bữa Trưa Hải Sản</li>
-                                </ul>
-                            </div>
+                        <ul>
+                            <li>Lướt cano trên biển</li>
+                            <li>Ngắm San Hô</li>
+                            <li>Bữa Trưa Hải Sản</li>
+                        </ul>
+                    </div>
                 </div>
                 <div class="col-md-4 text-end">
                     <p class=" text-centermoretour">Khởi hành: 31-10-2021</p>
@@ -88,62 +86,249 @@ include('header.php');?>
                     <p class=" textgray-moretour">*Áp dụng nhóm 1 khách</p>
                 </div>
             </div>
-
-            <div class="container">
-                <select class="form-select" aria-label="Default select example">
-                    <option selected> Tours trong nước</option>
+            <div class="row row-white">
+                <div class="col-md-3">
                     <a href="">
-                        <option value="Tour Canada">One </option>
+                        <img class="img-moretour" src="../img/da-lat.jpg" alt="dalat">
                     </a>
-                    <a href="">
-                        <option value="Tour France">Two</option>
-                    </a>
-                    <a href="">
-                        <option value="Tour American">Three</option>
-                    </a>
-
-
-
-                </select>
-                <select class="form-select" aria-label="Default select example">
-                    <option selected> Tours nước ngoài</option>
-                    <option value="1">One</option>
-                    <option value="2">Two</option>
-                    <option value="3">Three</option>
-                </select>
-            </div>
-
-
-            <div class="col col-huyen">
-                <a href="">
-                    <div class="card" style="width: 22rem;">
-                        <div class="huyen-img">
-                            <img src="../img/CANADA.jpg" class="card-img-top" alt="CANADA">
-                            <div class="huyen-card">
-                                <i class="fas fa-map-marker-alt "></i>
-                                Canada
-                            </div>
-                        </div>
-
-                        <div class="card-body">
-                            <h5 class="card-title">Tour Canada</h5>
-                            <p class="card-text">
-                            <div class="row">
-                                <div class="col-md-2 score" style="max-width:50px">9.6</div>
-                                <div class="col-md-4 score-des">Tuyệt vời</div>
-                                <div class="col-md comment"> | 5 đánh giá</div>
-                            </div>
-                            <div class="row ">
-                                <div class="col-md-10"><i class="far fa-clock"></i> 2 ngày 1 đêm </div>
-                                <div class="col-md-2"><i class="fas fa-plane"></i></div>
-                            </div>
-                            </p>
-                            <a href="#" class="col price">87.900.000</a>
-                        </div>
-
+                </div>
+                <div class="col-md-5">
+                    <div class="row">
+                        <a href="" class="linkdetail">
+                            Tour Đà Lạt 1N1D: Cắm Trại Cây Thông Cô Đơn
+                        </a>
                     </div>
-                </a>
+                    <div class="row">
+                        <div class="col-md-2 score" style="max-width:50px">10.0</div>
+                        <div class="col-md-3 score-des">Tuyệt vời</div>
+                        <div class="col-md comment"> | 3 đánh giá</div>
+                    </div>
+                    <div class="row ">
+                        <div class="col-md-4">Mã: TO1316</div>
+                        <div class="col-md-6"><i class="far fa-clock"></i>trong ngày</div>
+                        <div class="col-md-1"><i class="fas fa-car-alt"></i></div>
+                    </div>
+                    <div class="row  list_moretour">
+                        <ul>
+                            <li>Ngắm Bình Minh</li>
+                            <li>Săn Mây Sáng Sớm</li>
+                            <li>Vườn Dâu Tây</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-4 text-end">
+                    <p class=" text-centermoretour">Khởi hành: 31-10-2021</p>
+                    <h4 class=" price-moretour">400.000</h4>
+                    <p class=" textgray-moretour">*Áp dụng nhóm 1 khách</p>
+                </div>
             </div>
+            <div class="row row-white">
+                <div class="col-md-3">
+                    <a href="">
+                        <img class="img-moretour" src="../img/nha-trang.jpg" alt="dalat">
+                    </a>
+                </div>
+                <div class="col-md-5">
+                    <div class="row">
+                        <a href="" class="linkdetail">
+                            Tour Nha Trang 1N: Khám Phá Vịnh San Hô 2 - Tắm Bùn Hòn Tằm
+                        </a>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-2 score" style="max-width:50px">8.0</div>
+                        <div class="col-md-3 score-des">Rất tốt </div>
+                        <div class="col-md comment"> | 1 đánh giá</div>
+                    </div>
+                    <div class="row ">
+                        <div class="col-md-4">Mã: TO1429</div>
+                        <div class="col-md-6"><i class="far fa-clock"></i>trong ngày</div>
+                        <div class="col-md-1"><i class="fas fa-car-alt"></i></div>
+                        <div class="col-md-1"><i class="fas fa-subway"></i></div>
+                    </div>
+                    <div class="row  list_moretour">
+                        <ul>
+                            <li>Trải Nghiệm Vịnh San Hô 2</li>
+                            <li>Tắm Bùn Khoáng</li>
+                            <li>Thỏa Sức Party Hồ Bơi</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-4 text-end">
+                    <p class=" text-centermoretour">Khởi hành: 31-10-2021</p>
+                    <h4 class=" price-moretour">650.000</h4>
+                    <p class=" textgray-moretour">*Áp dụng nhóm 1 khách</p>
+                </div>
+            </div>
+            <div class="row row-white">
+                <div class="col-md-3">
+                    <a href="">
+                        <img class="img-moretour" src="../img/phu-quoc.jpg" alt="dalat">
+                    </a>
+                </div>
+                <div class="col-md-5">
+                    <div class="row">
+                        <a href="" class="linkdetail">
+                            Tour Phú Quốc 1N: Vi Vu Cano 5 Đảo - Cáp Treo Hòn Thơm
+                        </a>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-2 score" style="max-width:50px">10.0</div>
+                        <div class="col-md-3 score-des">Tuyệt vời </div>
+                        <div class="col-md comment"> | 1 đánh giá</div>
+                    </div>
+                    <div class="row ">
+                        <div class="col-md-4">Mã: TO1523</div>
+                        <div class="col-md-6"><i class="far fa-clock"></i>trong ngày</div>
+                        <div class="col-md-1"><i class="fas fa-car-alt"></i></div>
+                        <div class="col-md-1"><i class="fas fa-subway"></i></div>
+                    </div>
+                    <div class="row  list_moretour">
+                        <ul>
+                            <li>Cano 5 Đảo</li>
+                            <li>Cap Treo Hòn Thơm</li>
+                            <li>Tặng Free Bộ Ảnh Chụp Tại Đảo</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-4 text-end">
+                    <p class=" text-centermoretour">Khởi hành: 31-10-2021</p>
+                    <h4 class=" price-moretour">1.100.000</h4>
+                    <p class=" textgray-moretour">*Áp dụng nhóm 1 khách</p>
+                </div>
+            </div>
+            <div class="row row-white">
+                <div class="col-md-3">
+                    <a href="">
+                        <img class="img-moretour" src="../img/hoang-hon.jpg" alt="dalat">
+                    </a>
+                </div>
+                <div class="col-md-5">
+                    <div class="row">
+                        <a href="" class="linkdetail">
+                            Tour Đà Lạt 1/2N: Chèo SUP Ngắm Hoàng Hôn Hồ Tuyền Lâm
+                        </a>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-2 score" style="max-width:50px">10.0</div>
+                        <div class="col-md-3 score-des">Tuyệt vời </div>
+                        <div class="col-md comment"> | 3 đánh giá</div>
+                    </div>
+                    <div class="row ">
+                        <div class="col-md-4">Mã: TO1578</div>
+                        <div class="col-md-6"><i class="far fa-clock"></i>trong ngày</div>
+                        <div class="col-md-1"><i class="fas fa-car-alt"></i></div>
+                        <div class="col-md-1"><i class="fas fa-subway"></i></div>
+                    </div>
+                    <div class="row  list_moretour">
+                        <ul>
+                            <li>Trải Nghiệm Chèo SUP</li>
+                            <li>Ngắm Hoàng Hôn Hồ Tuyền Lâm</li>
+                            <li>Tặng Free Bộ Ảnh Chụp</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-4 text-end">
+                    <p class=" text-centermoretour">Khởi hành: 31-10-2021</p>
+                    <h4 class=" price-moretour">430.000</h4>
+                    <p class=" textgray-moretour">*Áp dụng nhóm 1 khách</p>
+                </div>
+            </div>
+            <div class="row row-white">
+                <div class="col-md-3">
+                    <a href="">
+                        <img class="img-moretour img-fluid" src="../img/dalat.jpg" alt="phuquoc">
+                    </a>
+                </div>
+                <div class="col-md-6">
+                    <div class="row">
+                        <a href="" class="linkdetail">
+                            Tour Phú Quốc 1N: Xe Jeep Khám Phá Nam Đảo
+                        </a>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-2 score" style="max-width:50px">10.0</div>
+                        <div class="col-md-3 score-des">Tuyệt vời </div>
+                        <div class="col-md comment"> | 1 đánh giá</div>
+                    </div>
+                    <div class="row ">
+                        <div class="col-md-4">Mã: TO9823</div>
+                        <div class="col-md-6"><i class="far fa-clock"></i>trong ngày</div>
+                        <div class="col-md-1"><i class="fas fa-car-alt"></i></div>
+                    </div>
+                    <div class="row  list_moretour">
+                        <ul>
+                            <li>Trải Nghiệm Xe Jeep</li>
+                            <li>Khám Phá Nam Đảo Phú Quốc</li>
+                            <li>Trại Rắn Đồng Tâm 2</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-3 text-end">
+                    <p class=" text-centermoretour">Khởi hành: 31-10-2021</p>
+                    <h4 class=" price-moretour">800.000</h4>
+                    <p class=" textgray-moretour">*Áp dụng nhóm 1 khách</p>
+                </div>
+            </div>
+            <div class="row row-white">
+                <div class="col-md-3">
+                    <a href="">
+                        <img class="img-moretour" src="../img/chua-phuquoc.jpg" alt="chua-phuquoc">
+                    </a>
+                </div>
+                <div class="col-md-5">
+                    <div class="row">
+                        <a href="" class="linkdetail">
+                            Tour Phú Quốc 1N: xe Jeep Khám Phá Bắc Đảo
+                        </a>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-2 score" style="max-width:50px">10.0</div>
+                        <div class="col-md-3 score-des">Tuyệt vời </div>
+                        <div class="col-md comment"> | 1 đánh giá</div>
+                    </div>
+                    <div class="row ">
+                        <div class="col-md-4">Mã: TO1983</div>
+                        <div class="col-md-6"><i class="far fa-clock"></i>trong ngày</div>
+                        <div class="col-md-1"><i class="fas fa-car-alt"></i></div>
+                        <div class="col-md-1"><i class="fas fa-plane"></i></div>
+                    </div>
+                    <div class="row  list_moretour">
+                        <ul>
+                            <li>Trải Nghiệm Xe Jeep</li>
+                            <li>Rừng Nguyên Sinh Phú Quốc</li>
+                            <li>Thử Lái Xe Jeep Trong Rừng</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-4 text-end">
+                    <p class=" text-centermoretour">Khởi hành: 31-10-2021</p>
+                    <h4 class=" price-moretour">950.000</h4>
+                    <p class=" textgray-moretour">*Áp dụng nhóm 1 khách</p>
+                </div>
+            </div>
+
+            <div class="select">
+                <div class="row ">
+                    <div class="col-md-6">
+                        <select class="form-select bg-light" aria-label="Default select example">
+                            <option selected> <a href="">Tours nước ngoài</a></option>
+                            <option value="1"><a href="">Canada</a></option>
+                            <option value="2"><a href="">America</a></option>
+                            <option value="3"><a href="">Germany</a></option>
+                        </select>
+                    </div>
+                    <div class="col-md-6">
+                        <select class="form-select bg-light" aria-label="Default select example">
+                            <option selected> <a href="">Tours trong nước</a></option>
+                            <option value="1"><a href="">Phú Quốc</a></option>
+                            <option value="2"><a href="">Đà Lạt</a></option>
+                            <option value="3"><a href="">Nha Trang</a></option>
+                            <option value="1"><a href="">Phan Thiết</a></option>
+                        </select>
+                     </div>
+                </div>
+            </div>
+
         </div>
     </div>
 </div>
