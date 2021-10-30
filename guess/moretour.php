cc048fbb6252b4b8616dbc27f7068b606eb7f828
nguyenthikhanhhuyen <nguyent.khanhhuyen309@gmail.com>
2021-10-30 08:29:27 +0700

    tao gd moretour

---
+++ guess/moretour.php
@@ -0,0 +1,154 @@
+<?php
+include('header.php');?>
+<!--  -->
+
+
+<!--  -->
+<div class="my_moretour" style="overflow: hidden">
+    <div class="container">
+
+        <div class="row ">
+            <h3 class="header-moretour">
+                Trải Nghiệm Địa Phương từ Hồ Chí Minh
+            </h3>
+        </div>
+
+        <div class="container">
+            <div class="row row-white">
+                <div class="col-md-3">
+                    <a href="">
+                        <img class="img-moretour" src="../img/cu-lao-xanhjpg.jpg" alt="cu-lao-xanh">
+                    </a>
+                </div>
+                <div class="col-md-5">
+                    <div class="row">
+                        <a href=""
+                            class="linkdetail">
+                            Tour Quy Nhơn 1N: Đảo Ngọc Cù Lao Xanh
+                        </a>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-2 score" style="max-width:50px">9.6</div>
+                        <div class="col-md-3 score-des">Tuyệt vời</div>
+                        <div class="col-md comment"> | 5 đánh giá</div>
+                    </div>
+                    <div class="row ">
+                        <div class="col-md-4">Mã: TO1242</div>
+                        <div class="col-md-6"><i class="far fa-clock"></i>2 ngày 1 đêm</div>
+                        <div class="col-md-1"><i class="fas fa-car-alt"></i></div>
+                        <div class="col-md-1"><i class="fas fa-plane"></i></div>
+                    </div>
+                    <div class="row  list_moretour">
+                                <ul>
+                                    <li>Bữa Trưa Hải Sản</li>
+                                    <li>Bãi đá Thảo Nguyên</li>
+                                    <li>Ngọn Hải Đăng 120 tuổi</li>
+                                </ul>
+                            </div>
+                </div>
+                <div class="col text-end ">
+                    <p class="text-gray">Hết chỗ</p>
+                </div>
+            </div>
+                <div class="col-md-3">
+                    <a href="">
+                        <img class="img-moretour" src="../img/cano.jpg" alt="cano">
+                    </a>
+                </div>
+                <div class="col-md-5">
+                    <div class="row">
+                        <a href=""
+                            class="linkdetail">
+                            Tour Cù Lao Chàm 1N: Đi Bộ Dưới Đáy Biển Cano                                                        
+                        </a>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-2 score" style="max-width:50px">9</div>
+                        <div class="col-md-3 score-des">Tuyệt vời</div>
+                        <div class="col-md comment"> | 1 đánh giá</div>
+                    </div>
+                    <div class="row ">
+                        <div class="col-md-4">Mã: TO1289</div>
+                        <div class="col-md-6"><i class="far fa-clock"></i>trong ngày</div>
+                        <div class="col-md-1"><i class="fas fa-car-alt"></i></div>
+                        <div class="col-md-1"><i class="fas fa-subway"></i></div>
+                    </div>
+                    <div class="row  list_moretour">
+                                <ul>
+                                    <li>Lướt cano trên biển</li>
+                                    <li>Ngắm San Hô</li>
+                                    <li>Bữa Trưa Hải Sản</li>
+                                </ul>
+                            </div>
+                </div>
+                <div class="col-md-4 text-end">
+                    <p class=" text-centermoretour">Khởi hành: 31-10-2021</p>
+                    <h4 class=" price-moretour">1.450.000</h4>
+                    <p class=" textgray-moretour">*Áp dụng nhóm 1 khách</p>
+                </div>
+            </div>
+
+            <div class="container">
+                <select class="form-select" aria-label="Default select example">
+                    <option selected> Tours trong nước</option>
+                    <a href="">
+                        <option value="Tour Canada">One </option>
+                    </a>
+                    <a href="">
+                        <option value="Tour France">Two</option>
+                    </a>
+                    <a href="">
+                        <option value="Tour American">Three</option>
+                    </a>
+
+
+
+                </select>
+                <select class="form-select" aria-label="Default select example">
+                    <option selected> Tours nước ngoài</option>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                </select>
+            </div>
+
+
+            <div class="col col-huyen">
+                <a href="">
+                    <div class="card" style="width: 22rem;">
+                        <div class="huyen-img">
+                            <img src="../img/CANADA.jpg" class="card-img-top" alt="CANADA">
+                            <div class="huyen-card">
+                                <i class="fas fa-map-marker-alt "></i>
+                                Canada
+                            </div>
+                        </div>
+
+                        <div class="card-body">
+                            <h5 class="card-title">Tour Canada</h5>
+                            <p class="card-text">
+                            <div class="row">
+                                <div class="col-md-2 score" style="max-width:50px">9.6</div>
+                                <div class="col-md-4 score-des">Tuyệt vời</div>
+                                <div class="col-md comment"> | 5 đánh giá</div>
+                            </div>
+                            <div class="row ">
+                                <div class="col-md-10"><i class="far fa-clock"></i> 2 ngày 1 đêm </div>
+                                <div class="col-md-2"><i class="fas fa-plane"></i></div>
+                            </div>
+                            </p>
+                            <a href="#" class="col price">87.900.000</a>
+                        </div>
+
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!--  -->
+
+<?php
+include('footer.php');
+?>
