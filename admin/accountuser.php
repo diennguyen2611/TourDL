4fcbe9b6a95793da14d304aa75b28545932b8aed
thuha-286 <nthuha0608@gmail.com>
2021-11-02 17:03:41 +0700

    đẩy dl bảng người dùng

--- admin/accountuser.php
+++ admin/accountuser.php
@@ -1,33 +1,32 @@
-php 
+<?php 
 
 include('header.php');
-include('..configconnect.php');
+include('../config/connect.php');
+?>
+<div class="container my-main">
 
-div class=container my-main
-
-    h2 class=text-uppercase my-title-adQuản lý người dùngh2
-    table class=table table-striped
-        thead
-            tr
-                th scope=colSTTth
-                th scope=coluserIDth
-                th scope=coluserNameth
-                th scope=coluserPassth
-                th scope=coluserEmailth
-                th scope=coluserCodeth
-                th scope=coluserPhoneth
-                th scope=colregistration_dateth
-                th scope=coluserStatusth
-                th scope=colactionth
-            tr
-        thead
-        tbody
-
-        php
-            $sql = select   from users;
+    <h2 class="text-uppercase my-title-ad">Quản lý tours</h2>
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th scope=col>STT</th>
+                <th scope=col>userID</th>
+                <th scope=col>userName</th>
+                <th scope=col>userPass</th>
+                <th scope=col>userEmail</th>
+                <th scope=col>userCode</th>
+                <th scope=col>userPhone</th>
+                <th scope=col>registration_date</th>
+                <th scope=col>userStatus</th>
+                <th scope=col>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+        <?php
+            $sql = "select * from users";
             $result = mysqli_query($conn,$sql);
             
-            if(mysqli_num_rows($result)0){
+            if(mysqli_num_rows($result)>0){
                 $STT=1;
                 while($row=mysqli_fetch_assoc($result)){
                     $userID=$row['userID'];
@@ -37,36 +36,53 @@ div class=container my-main
                     $userCode=$row['userCode'];
                     $userPhone=$row['userPhone'];
                     $registration_date=$row['registration_date'];
-                    $userStatus=$row['userStatus'];
-tr
-                th scope=rowphp echo $STT++th
-                tdphp echo $userIDtd
-                tdphp echo $userNametd
-                tdphp echo $userPasstd
-                tdphp echo $userEmailtd
-                tdphp echo $userCodetd
-                tdphp echo $userPhonetd
-                tdphp echo $registration_datetd
-                tdphp echo $userStatustd
+                    $userStatus=$row['userStatus'];?>
+                    <tr>
+                <th scope=rowphp><?php echo $STT++?></th>
+                <td><?php echo $userID?></td>
+                <td><?php echo $userName?></td>
+                <td><?php echo $userPass?></td>
+                <td><?php echo $userEmail?></td>
+                <td><?php echo $userCode?></td>
+                <td><?php echo $userPhone?></td>
+                <td><?php echo $registration_date?></td>
+                <td><?php echo $userStatus?></td>
 
-                td
-                    a href=deleteuser.phpid=php echo $userID; 
-                        onclick=return confirm('Bạn chắc chắn muốn xóa')button type=button
-                            class=btn btn-dangerXóabuttona
-                td
-
-
-            tr
-         php       }
+                <td>
+                <a href="edituser.php?id=<?php echo $userID; ?>"><button type="button"
+                            class="btn btn-success">Sửa</button></a>
+                </tr>
+         <?php       }
             } else echo $sql;
-        
+        ?>
 
-            
-        tbody
-    table
-div
-
+        </tbody>
+    </table>
+</div>
 
 
-php
+
+<?php
 include('footer.php');
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
